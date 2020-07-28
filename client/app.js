0734bf284ce0a2533b16a1ed9b62a56607fb8d3a
Shruti Pahadiya <15.shruti@gmail.com>
2020-07-28 11:20:41 -0400

    NAV and css added

--- client/app.js
+++ client/app.js
@@ -1,10 +1,18 @@
 //  Different from index.js file - we can use the Provider, BrowserRouter, Nav, Switch here
 import React, { Component } from 'react';
+import {HashRouter,Route} from 'react-router-dom';
+import Nav  from './components/Nav';
 
 export default class App extends Component {
     render() {
         return (
             <div>
+            <HashRouter>
+          <div>
+              <Route component={Nav} />
+             </div>
+          </HashRouter>
+           
                 <h1>This is DVDs R Us!</h1>
             </div>
         )
---
+++ client/components/nav.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+    return (
+      <div>
+        <nav className="navDiv">
+          <Link to="/search" style={{color: "white"}} > Search</Link>
+          <Link to="/browse" style={{color: "white" }}>Browse</Link>
+          <Link to="/yourOrders"  style={{color: "white"}}>Your Orders</Link>
+          <Link to="/cart" style={{color: "white" }} > Cart </Link>
+        </nav>
+      </div>
+    );
+  }
+
+
+// const mapStateToProps = (state) => {
+//   return { };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {};
+// };
+
+export default (Nav);
