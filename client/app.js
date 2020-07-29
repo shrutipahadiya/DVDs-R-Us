a1a437f5f9473e3fa5733c2475b513345ea4e6fb
nickcarr51 <nick.carr84@gmail.com>
2020-07-29 12:30:27 -0400

    moviepage and basic browse

--- client/app.js
+++ client/app.js
@@ -3,23 +3,25 @@ import React, { Component } from "react";
 import { HashRouter, Route,Switch,Redirect } from "react-router-dom";
 import Nav from "./components/Nav";
 import Search from './components/search';
+import Browse from './components/browse';
+import MoviePage from './components/moviePage';
 
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className='container'>
         <HashRouter>
           <div>
             <Route component={Nav} />
-            </div>
-            <Switch>
-              <Route exact path='/search' render={()=><Search/>}/>
-              <Redirect to='/'/>
-              </Switch>
+          </div>
+          <Switch>
+            <Route exact path='/search' render={()=><Search/>}/>
+            <Route exact path='/browse' render={(props) => <Browse props={ props } />}/>
+            <Route path='/browse/:id?' render={(props) => <MoviePage props={ props } />}/>
+            <Redirect to='/'/>
+          </Switch>
         </HashRouter>
-
-        <h1>This is DVDs R Us!</h1>
       </div>
     );
   }
