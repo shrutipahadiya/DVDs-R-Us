015485101d55ee96a2897b9875bdc88832274c49
nickcarr51 <nick.carr84@gmail.com>
2020-07-29 21:49:41 -0400

    added react-youtube

--- client/components/About.js
+++ client/components/About.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import YouTube from 'react-youtube';
+
+const YOUTUBE_ID = 'Zc4uTz9-Q1Q'
 
 const About = () => {
-    return (
+
+    const opts = {
+        height: '390',
+        width: '640',
+        playerVars: {
+          autoplay: 1,
+        }
+    }
+    return ( 
         <div>
-            <h2>THIS IS DVD!</h2>
-            <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/Zc4uTz9-Q1Q"
-                frameborder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen>
-            </iframe>
+            <h2 className='title is-2' style={{textAlign:'center'}}>THIS IS DVD!</h2>
+            <div className='box' style={{display:'flex', justifyContent:'space-around'}}>
+                <YouTube videoId={ YOUTUBE_ID } opts={opts} />
+            </div>
         </div>
     )
 }
