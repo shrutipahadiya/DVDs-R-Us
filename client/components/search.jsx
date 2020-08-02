7884e1b70abda43626a7e56018d5c32924f58967
Shruti Pahadiya <15.shruti@gmail.com>
2020-08-02 19:04:29 -0400

    search again

--- client/components/search.jsx
+++ client/components/search.jsx
@@ -2,47 +2,142 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
 import { getMovies } from '../redux/movies/actions';
+import { movieFilter } from '../utilities';
+import singleMovieBox from './singleMovieBox';
 
 class Search extends Component {
   constructor() {
     super();
     this.state = {
-      // searchTerm: '',
+      searchTerm: '',
+      searchCriteria: 'title',
+      searchedMovies: [],
     };
   }
 
   async componentDidMount() {
-    // console.log('component mounted ');
-    // const { getMovies } = this.props;
-    // await getMovies();
-    // await getMovies();
-    await this.props.getMovies();
-    // console.log('getMovies called here --', this.props);
+    // eslint-disable-next-line no-shadow
+    const { getMovies } = this.props;
+    await getMovies();
+  }
+
+  handleSearchTermChange(ev) {
+    this.setState({ [ev.target.name]: ev.target.value });
+  }
+
+  handleSearchCriteriaChange(ev) {
+    this.setState({ [ev.target.name]: ev.target.value });
+  }
+
+  handleSearch(ev) {
+    ev.preventDefault();
+    const { searchTerm, searchCriteria } = this.state;
+    this.searchMovies(searchTerm, searchCriteria);
+  }
+
+  searchMovies(searchTerm, searchCriteria) {
+    let { movies } = this.props;
+    movies = movieFilter(movies, searchCriteria, 'A-Z', searchTerm);
+    this.setState({ searchedMovies: movies });
   }
 
   render() {
-    // console.log('props in search ', this.props);
+    const { searchTerm, searchCriteria, searchedMovies } = this.state;
     return (
-      <form>
-        <div>
-          <h2>SEARCH </h2>
-          <input type="text" placeholder="Enter Your Search Term Here" name="searchTerm" />
-        </div>
-      </form>
+      <div>
+        <form>
+          <div>
+            <label htmlFor="searchTerm"> Search Term:</label>
+            <input
+              type="text"
+              id="searchTerm"
+              placeholder="Enter Your Search Term Here"
+              value={searchTerm}
+              name="searchTerm"
+              onChange={(ev) => this.handleSearchTermChange(ev)}
+            />
+          </div>
+          <div>
+            <label htmlFor="searchCriteria">Search Criteria:</label>
+            <select
+              value={searchCriteria}
+              id="searchCriteria"
+              name="searchCriteria"
+              onChange={(ev) => this.handleSearchCriteriaChange(ev)}
+            >
+              <option value="actors" key="actors">
+                Actor
+              </option>
+              <option value="awards" key="awards">
+                Awards
+              </option>
+              <option value="boxoffice" key="boxoffice">
+                Boxoffice
+              </option>
+              <option value="director" key="director">
+                Director
+              </option>
+              <option value="genres" key="genres">
+                Genre
+              </option>
+              <option value="metascore" key="metascore">
+                Metascore
+              </option>
+              <option value="plot" key="plot">
+                Plot
+              </option>
+              <option value="production" key="production">
+                Production
+              </option>
+              <option value="rating" key="rating">
+                Rating
+              </option>
+              <option value="released" key="released">
+                Released
+              </option>
+              <option value="runtime" key="runtime">
+                Runtime
+              </option>
+              <option value="title" key="title">
+                Title
+              </option>
+              <option value="writer" key="writer">
+                Writer
+              </option>
+              <option value="year" key="year">
+                Year
+              </option>
+            </select>
+          </div>
+          <button type="button" onClick={(ev) => this.handleSearch(ev)}>
+            Search
+          </button>
+        </form>
+
+        <ul>
+          {
+          searchedMovies
+            ? searchedMovies.map((movie) => (
+              singleMovieBox(movie)
+            ))
+            : null
+             }
+        </ul>
+      </div>
+
     );
   }
 }
 
 Search.propTypes = {
   getMovies: propTypes.func.isRequired,
+  movies: propTypes.arrayOf(propTypes.object).isRequired,
 };
 
 const mapStateToProps = (state) => ({
   movies: state.movieReducer.movies,
 });
 
-const mapDispatchToProps = {
-  getMovies,
-};
+const mapDispatchToProps = { getMovies };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Search);
--- client/utilities.js
+++ client/utilities.js
@@ -10,26 +10,76 @@ export const generateGenres = (movies) => {
   return genreArray;
 };
 
-export const movieFilter = (movies, filter, sort) => {
+export const movieFilter = (movies, filter, sort, searchTerm) => {
   // let filteredMovies = movies;
   let filteredMovies = movies;
   switch (filter) {
     case 'All':
       filteredMovies = filteredMovies; // eslint-disable-line no-self-assign
       break;
+    case 'title':
+      filteredMovies = movies.filter((movie) => movie.title.toLowerCase()
+        .includes(searchTerm.toLowerCase()));
+      break;
+    case 'year':
+      filteredMovies = movies.filter((movie) => movie.year.toString().includes(searchTerm));
+      break;
+    case 'director':
+      filteredMovies = movies.filter((movie) => movie.director
+        .toString()
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase()));
+      break;
+    case 'actors':
+      filteredMovies = movies.filter((movie) => movie.actors.toString().toLowerCase()
+        .includes(searchTerm.toLowerCase()));
+      break;
+    case 'awards':
+      filteredMovies = movies.filter((movie) => movie.awards.toLowerCase()
+        .includes(searchTerm.toLowerCase()));
+      break;
+    case 'boxoffice':
+      filteredMovies = movies.filter((movie) => movie.boxoffice.toString().includes(searchTerm));
+      break;
+    case 'metascore':
+      filteredMovies = movies.filter((movie) => movie.metascore.toString().includes(searchTerm));
+      break;
+    case 'plot':
+      filteredMovies = movies.filter((movie) => movie.plot.toLowerCase()
+        .includes(searchTerm.toLowerCase()));
+      break;
+    case 'rating':
+      filteredMovies = movies.filter((movie) => movie.rating.toString().includes(searchTerm));
+      break;
+    case 'production':
+      filteredMovies = movies.filter((movie) => movie.production.toLowerCase()
+        .includes(searchTerm.toLowerCase()));
+      break;
+    case 'runtime':
+      filteredMovies = movies.filter((movie) => movie.runtime.toString().includes(searchTerm));
+      break;
+    case 'writer':
+      filteredMovies = movies.filter((movie) => movie.writer.toString().toLowerCase()
+        .includes(searchTerm.toLowerCase()));
+      break;
+    case 'released':
+      filteredMovies = movies.filter((movie) => movie.released.toString().includes(searchTerm));
+      break;
+    case 'genres':
+      filteredMovies = movies.filter((movie) => movie.genres.toString().toLowerCase()
+        .includes(searchTerm.toLowerCase()));
+      break;
     default:
       filteredMovies = filteredMovies.filter((movie) => movie.genres.includes(filter));
   }
   switch (sort) {
     case 'A-Z':
-      filteredMovies = filteredMovies.sort((a, b) => (
-        a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1
-      ));
+      filteredMovies = filteredMovies.sort((a, b) => (a.title.toLowerCase()
+                                     > b.title.toLowerCase() ? 1 : -1));
       break;
     case 'Z-A':
-      filteredMovies = filteredMovies.sort((a, b) => (
-        a.title.toLowerCase() < b.title.toLowerCase() ? 1 : -1
-      ));
+      filteredMovies = filteredMovies.sort((a, b) => (a.title.toLowerCase()
+                                     < b.title.toLowerCase() ? 1 : -1));
       break;
     case 'Highest Rated':
       filteredMovies = filteredMovies.sort((a, b) => (a.rating < b.rating ? 1 : -1));
